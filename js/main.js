228950f702090bf6eb0af3a39d0151d68178e106
geordangesink <georgegeordan@gmail.com>
2024-06-14 20:57:52 +0200

    added comments to main.js file

--- js/main.js
+++ js/main.js
@@ -1,34 +1,35 @@
 let video = document.querySelector( "#video" );
 let image = document.querySelector( "#image" );
+// Scene and Snippet number
 let sceneNum = 1;
 let cutNum = 1;
-
+// Start at this video when refreshing page
 video.src = `assets/videos/scene-${ sceneNum }-snippet-${ cutNum }.mp4`;
 
-// video.addEventListener( "ended", quiz );
 video.onended = quiz;
 
 
 function quiz(){
-
+    // If currently in scene 1 or 2 and not at the last snippet
     if ( ( sceneNum === 1 || sceneNum === 2 ) && cutNum < 4 ){
         
-        
+        // Hide video and show Screenshot
         video.classList.add( "hide" );
         image.src = `assets/images/scene-${ sceneNum }-snapshot-${ cutNum }.png`;
         image.classList.remove( "hide" );
 
+        // Next snippet
         cutNum++;
 
         document.onkeydown = nextVideo;
     }
-
+    // If at the end of scene 1 or 2
     else if ( cutNum >= 4 ){
         sceneNum++;
         cutNum = 1;
         nextVideo();
     }
-
+    // if at scene 3 (last video snippet)
     else{
         video.classList.add( "hide" );
         image.src = `assets/images/scene-${ 3 }-snapshot-${ 1 }.png`;
@@ -39,11 +40,10 @@ function quiz(){
         }
     }
 
-    // document.addEventListener( "keypress", nextVideo );
 }
 
 function nextVideo(){
-    // document.removeEventListener( "keypress", nextVideo );
+    // Hide Screenshot and show Next Video
     image.classList.add( "hide" );
     video.src = `assets/videos/scene-${ sceneNum }-snippet-${ cutNum }.mp4`;
     video.classList.remove( "hide" );
