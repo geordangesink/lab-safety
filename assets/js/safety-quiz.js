00bd920f8e62efba21341122e3083088e5ffce9e
geordangesink <georgegeordan@gmail.com>
2024-06-18 03:59:09 +0200

    -

--- assets/js/safety-quiz.js
+++ assets/js/safety-quiz.js
@@ -1,7 +1,7 @@
 let video = document.querySelector( "#video" );
 let image = document.querySelector( "#image" );
 // Scene and Snippet number
-let sceneNum = 1;
+let sceneNum = 0;
 let cutNum = 1;
 // Start at this video when refreshing page
 video.src = `assets/videos/scene-${ sceneNum }-snippet-${ cutNum }.mp4`;
@@ -10,8 +10,15 @@ video.onended = quiz;
 
 
 function quiz(){
+
+    if ( sceneNum === 0 && cutNum === 2 ){
+        sceneNum = 1;
+        cutNum = 1;
+        nextVideo();
+    }
+
     // If currently in scene 1 or 2 and not at the last snippet
-    if ( ( sceneNum === 1 || sceneNum === 2 ) && cutNum < 4 ){
+    else if ( ( sceneNum === 0 || sceneNum === 1 || sceneNum === 2 ) && cutNum < 4 ){
         
         // Hide video and show Screenshot
         video.classList.add( "hide" );
